612b508f4f6fa84c4921191a53d080193264e4f6
KentNa <kentcandoit89@gmail.com>
2025-01-26 23:55:45 +0900

    feat: 포폴 기획 및 구성 주석

--- src/main/java/com/master/side/application/service/TaskService.java
+++ src/main/java/com/master/side/application/service/TaskService.java
@@ -94,4 +94,41 @@ public class TaskService {
                         .collect(Collectors.toList()))
                 .build();
     }
+
+//    보드를  구성할 때, 어떻게 잘 짤까?, 인증 인가, 작성자, view 등등 고민한 흔적,
+//    전후 과정, 고민들,
+//    n + 1,
+//    id - uuid로 관리
+//    uuid로 관리 해서 식별 할 수 있는 키,
+//    in-memory db - singlside, 서버든 spring security든
+//    보드, 작성자 파일 IO
+//    스프링을 잘 썼느냐 까지,
+//
+//    초보 vs 중급자 보드 차이
+//    초보 - 요청 받음, 파일 저장 시 byte 배열로 clob blob , clob, blob 리턴 기본적인 구현
+//    중급 - aop를 써서 특정 method, 특정 packge 밑에 aspect 사용, 메서드의 전 후 처리 로그 찍기
+//
+//    common - 작성자, 시간 등등
+//    로그를 위해서, 사용해야 하는 다채로운 스프링 기술들
+//    웹플럭스 - 리액티브 프로그래밍
+//
+//    exception 깔끔하게 사용하는 방법,
+//    예외를 안하거나 exception e만 try catch 써서 사용하는 것은 지양
+//
+//    aop를 잘 사용하면 중복된 try catch를 제거할 수 있다
+//
+//    예상가능한 예외 - 이 클래스는 이 예외를 발생 시킨다 인지
+//
+//    httpStatus code 적절히 사용하기
+//
+//    자료형 - Json - message Converter 잭슨 라이브러리 - 프로퍼티
+//
+//    data parsing
+//
+//
+//    domain -
+//    redis, kafka, kubernetis,
+//
+//    스택 - ㅡㅡㅡ 보고 가는게 낫지 않나
+
 }
